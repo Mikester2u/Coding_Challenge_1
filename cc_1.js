ce6039b337916391d5921704ed160b3e4aa08420
Michael Rueda <michael.a.rueda22@gmail.com>
2024-05-17 18:55:38 -0400

    Implement and populate arrays for bills, tips, and totals.

--- cc_1.js
+++ cc_1.js
@@ -11,4 +11,25 @@ const testBill = 100;
 const calcTip = (testBill) => (testBill >= 50 && testBill <= 300) ? testBill * 0.15 : testBill * 0.20; //calcTip function
 const testTip = calcTip(testBill);
 
-console.log("For a bill of $" + testBill + ", the tip is $" + testTip + ".") //printed output 
+console.log("For a bill of $" + testBill + ", the tip is $" + testTip + "."); //printed output 
+
+//Step 4 
+const bills1 = [275, 40, 430];
+const bills2 = [125, 555, 44];
+
+const tips1 = bills1.map(calcTip);
+const tips2 = bills2.map(calcTip);
+
+const totals1 = bills1.map((bill, index) => bill + tips1[index]);
+const totals2 = bills2.map((bill, index) => bill + tips2[index]);
+
+console.log("Data Set 1:");
+bills1.forEach((bill, index) => {
+    console.log("The bill is $" + bill + ", the tip is $" + tips1[index] + " and the total is $" + totals1[index] +
+        ".");
+});
+
+console.log("Data Set 2:");
+bills2.forEach((bill, index) => {
+    console.log("the bill is $" + bill + ", the tip is $" + tips2[index] + " and the total is $" + totals2[index] + ".");
+});
